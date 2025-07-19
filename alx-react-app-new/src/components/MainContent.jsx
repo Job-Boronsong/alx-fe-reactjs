49d2d9c61b860563be2ae877224dd04da951a926
Job Boronsong <jboronsong@gmail.com>
2025-07-19 04:54:16 +0000

    update on MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,7 @@
 function MainContent() {
     return (
         <main style={{ padding: '20px', backgroundColor: '#eef2f3' }}>
-            <p style={{ fontSize: '18px', lineHeight: '1.6' }}>I love to visit New York, Paris, 
-                and Tokyo.</p>
+            <p style={{ fontSize: '18px', lineHeight: '1.6' }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
