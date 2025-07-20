2549756bee889fd3bf736b6808357490106c0b03
Job Boronsong <jboronsong@gmail.com>
2025-07-20 11:40:40 +0000

    commit changes

--- alx-react-app-props/src/components/UserInfo.jsx
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -1,7 +1,16 @@
-import UserDetails from './UserDetails';
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
-function UserInfo() {
-  return <UserDetails />;
+function UserProfile() {
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
 }
 
-export default UserInfo;
+export default UserProfile;
