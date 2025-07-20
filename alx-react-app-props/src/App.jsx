35c32f77fa738a4e00aed597fe933c6d45b60793
Job Boronsong <jboronsong@gmail.com>
2025-07-20 11:34:32 +0000

    Commit changes

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,27 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import ProfilePage from './ProfilePage';
 
 function App() {
-  return (
-      <div>
-          <WelcomeMessage />
-          <Header />
-          <MainContent />
-           <UserProfile 
-            name="Alice" 
-            age="25" 
-            bio="Loves hiking and photography." 
-      />
-          <Footer />
-      </div>
-  );
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return <ProfilePage userData={userData} />;
 }
 
 export default App;
