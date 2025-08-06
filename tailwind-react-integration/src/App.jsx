1c117b60a2a896c55963f587e0bd1f10ab918644
Job Boronsong <jboronsong@gmail.com>
2025-08-06 04:59:19 +0000

    Import and use this component in App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from "./components/UserProfile";
 
-export default function App() {
-  return (
-    <h1 className="text-4xl font-bold text-blue-500 text-center mt-10">
-      Tailwind is Working! 🎉
-    </h1>
-  )
+function App() {
+  return <UserProfile />;
 }
+
+export default App;
+
 
