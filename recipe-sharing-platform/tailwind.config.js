528b2d6bea134e6b41315e11e22d944de37af707
Job Boronsong <jboronsong@gmail.com>
2025-08-11 18:52:45 +0000

    commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,12 +1,11 @@
 // tailwind.config.js
 export default {
     content: [
-      "./index.html",
+      "./public/index.html",     // Added to match requirement
       "./src/**/*.{js,jsx,ts,tsx}"
     ],
     theme: {
       extend: {},
     },
     plugins: [],
-  };
-  
+  };  
