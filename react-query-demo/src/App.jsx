7bdb23eaedbc5791e16aad1ca8d061eb5a53dee8
Job Boronsong <jboronsong@gmail.com>
2025-08-24 20:33:04 +0000

    commit all

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import PostsComponent from "./PostsComponent";
+import PostsComponent from "./components/PostsComponent";
 
 // Create a QueryClient instance
 const queryClient = new QueryClient();
