f35d5a30beebce2014517cfdd21e39bcd4c07103
Job Boronsong <jboronsong@gmail.com>
2025-08-24 17:46:04 +0000

    commit

--- form-handling-react/vite.config.js
+++ form-handling-react/vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   esbuild: {
-    loader: 'jsx', // 👈 tells Vite to parse JSX in .js files
+    loader: 'jsx', // 👈 parse JSX in js files
+    include: /src\/.*\.(js|jsx)$/, // 👈 ensure Vite scans both
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx', // 👈 very important for .js JSX files
+      },
+    },
   },
 })
